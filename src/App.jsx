3024fc13197ab7724520d1d9905e9ef1ee964057
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-07-19 13:04:01 -0500

    Fix eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './UI/Home';
+import Menu from './Features/Menu/Menu';
+import Cart from './Features/Cart/Cart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+]);
+
 const App = () => {
-  return <div>Hello, Vite!</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
