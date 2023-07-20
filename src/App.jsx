aa72c4c13daf92809c962dd4ce0f0c81f7acd90b
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-07-20 12:55:52 -0500

    Create new Orders, Post to API Endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './UI/Home';
 import Menu, { loader as menuLoader } from './Features/Menu/Menu';
 import Cart from './Features/Cart/Cart';
-import CreateOrder from './Features/Order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './Features/Order/CreateOrder';
 import Order, { loader as orderLoader } from './Features/Order/Order';
 import AppLayout from './UI/AppLayout';
 import Error from './UI/Error';
+import { createOrder } from './Services/apiRestaurant';
 
 const router = createBrowserRouter([
   {
@@ -30,6 +33,7 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: '/order/:orderID',
