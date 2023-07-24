1bfbe5254f2b47ec1db621ce82a92b9548f61aa8
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-07-24 12:48:05 -0500

    Add reusable Link Button and Button with styles

---
+++ src/UI/LinkButton.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+const LinkButton = ({ children, to }) => {
+  return (
+    <Link
+      className='text-sm text-blue-500 hover:text-blue-600 hover:underline'
+      to={to}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LinkButton;
