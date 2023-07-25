4bcd80fc630fb3a7baff0d72fe050920118a975e
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-07-25 10:16:31 -0500

    Modify primary/secondary button CSS, Correct bug with imput CSS flex box, correct key property for cart items

--- src/Features/Cart/Cart.jsx
+++ src/Features/Cart/Cart.jsx
@@ -37,7 +37,7 @@ function Cart() {
 
       <ul className='mt-3 divide-y divide-stone-200 border-b'>
         {cart.map((item) => {
-          return <CartItem item={item} key={item.pizzaID} />;
+          return <CartItem item={item} key={item.pizzaId} />;
         })}
       </ul>
 
